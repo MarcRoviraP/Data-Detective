ab17f5197383577ff6da02688670a46db608035c
MarcRoviraP <marc@rovira.dev>
2026-04-20 12:50:41 +0200

    feat: add launchers for Linux and Windows

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+echo 🚀 Iniciando Data Detective...
+pip install -r requirements_utf8.txt
+python main.py
+pause
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "🚀 Iniciando Data Detective..."
+python3 -m pip install -r requirements_utf8.txt
+python3 main.py
